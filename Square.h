4947f010675236f6c06bd569962b6b7a2afb25eb
Ning Hou <houningjo@gmail.com>
2017-11-09 22:22:35 -0800

    Update Square.h

--- Square.h
+++ Square.h
@@ -1,5 +1,46 @@
+//
+//  Square.h
+//
+//  Created by Ning Hou on 11/4/17.
+//  Copyright © 2017 Ning Hou. All rights reserved.
+//
+
+/** The Square class creats a Square object that represents one square in the
+* sudoku puzzle. 
+*/
 
 #pragma once
 #include <stdio.h>
-#include <string>
-using namespace std;
+
+class Square{
+
+private:
+    int value = 0;
+    bool isEmpty = true;
+
+protected:
+    /** Sets the value of this Square to the specified value n.
+    * @param n the specified value to be set
+    */
+    void setValue(int n);
+
+public:
+	  // constructors
+    /** Default constructor, sets this Square's value to 0 and isEmpty to true.
+    */
+    Square();
+
+    /** Returns true if this Square is empty, false otherwise.
+    * @return bool true if this Square is empty, false otherwise.
+    */
+    bool getIsEmpty() const;
+
+    /** Returns the value of this Square.
+    * @return the value of this Square. 
+    */
+    int getValue() const;
+
+    /** Displays a string representation of this Square object.
+    */
+    void toString() const;
+};
