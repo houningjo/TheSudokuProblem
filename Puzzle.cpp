19a56294fb105daa219e8f01213d228b1987196b
Ning Hou <houningjo@gmail.com>
2017-11-11 23:35:47 -0800

    Update Puzzle.cpp

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -6,10 +6,9 @@
 //
 
 /** The Puzzle class creates a Puzzle object that is made of
-* 9-by-9 Squares. 
-*/
+ * 9-by-9 Squares.
+ */
 
-#pragma once
 #include <stdio.h>
 #include <iostream>
 #include "Puzzle.h"
@@ -19,20 +18,22 @@ using namespace std;
 Puzzle::Puzzle(){
 }
 
-/** Returns true if the specified value is not equal to any 
-* value in the current block, false otherwise. 
-* (x, y) is the location of the left corner of the current Block. 
-* @pre value must be in range from 0 to 9.
-* @param x the column number of the loction of the left corner
-* @param y the row number of the loction of the left corner
-* @param value the specified value
-* @return bool true if the specified value is not equal to any
-* value in the current block, false otherwise
-*/
-bool Puzzle::isBlockLegal(int y, int x, int value){
-    for(int row = y; row < 3; row++){
-        for(int col = x; col < 3; col++){
-            if(puzzleTable[row][col].getValue() == value){
+/** Returns true if the specified value is not equal to any
+ * value in the current block, false otherwise.
+ * (row, col) is the location of the left corner of the current Block.
+ * @pre value must be in range from 0 to 9.
+ * @param row the row number of the loction of the left corner
+ * @param col the column number of the loction of the left corner
+ * @param value the specified value
+ * @return bool true if the specified value is not equal to any
+ * value in the current block, false otherwise
+ */
+bool Puzzle::isBlockLegal(int row, int col, int value){
+    for(int i = row; i < row + BLOCK_LENGTH; i++){
+        for(int j = col; j < col + BLOCK_LENGTH; j++){
+            if(puzzleTable[i][j].getValue() == value){
+                cout << "square value: " << puzzleTable[i][j].getValue() << endl;
+                cout << "block legal value: " << value << endl;
                 return false;
             }
         }
@@ -41,17 +42,17 @@ bool Puzzle::isBlockLegal(int y, int x, int value){
 }
 
 /** Returns true if the specified value is not equal to any value
-* in the current row, false otherwise.
-* @pre value must be in range from 0 to 9.
-* @param y the row number of the current row
-* @param value the specified value
-* @return bool true if the specifed value is not equal to any value
-* in the current row, false otherwise
-*/
-bool Puzzle::isRowLegal(int y, int x, int value){
+ * in the current row, false otherwise.
+ * @pre value must be in range from 0 to 9.
+ * @param row the row number of the current row
+ * @param value the specified value
+ * @return bool true if the specifed value is not equal to any value
+ * in the current row, false otherwise
+ */
+bool Puzzle::isRowLegal(int row, int value){
 
     for(int col = 0; col < PUZZLE_LENGTH; col++){
-        if(puzzleTable[y][col].getValue() == value){
+        if(puzzleTable[row][col].getValue() == value){
             return false;
         }
     }
@@ -59,16 +60,16 @@ bool Puzzle::isRowLegal(int y, int x, int value){
 }
 
 /** Returns false if the specified value is not equal to any value
-* in the current column, false otherwise.
-* @pre value must be in range from 0 to 9.
-* @param x the column number of the current colunmn
-* @param value the specified value
-* @return bool true if the specified value is not equal to any value
-* in the current column, false otherwise
-*/
-bool Puzzle::isColumnLegal(int y, int x, int value){
+ * in the current column, false otherwise.
+ * @pre value must be in range from 0 to 9.
+ * @param col the column number of the current colunmn
+ * @param value the specified value
+ * @return bool true if the specified value is not equal to any value
+ * in the current column, false otherwise
+ */
+bool Puzzle::isColumnLegal(int col, int value){
     for(int row = 0; row < PUZZLE_LENGTH; row++){
-        if(puzzleTable[row][x].getValue() == value){
+        if(puzzleTable[row][col].getValue() == value){
             return false;
         }
     }
@@ -76,42 +77,43 @@ bool Puzzle::isColumnLegal(int y, int x, int value){
 }
 
 /** Returns a const reference to the square at the specified location.
-* @param x the column number
-* @param y the row number
-* @return Square& 
-*/
-const Square& Puzzle::get(int y, int x){
-    return puzzleTable[y][x];
+ * @param row the row number
+ * @param col the col number
+ * @return Square&
+ */
+const Square& Puzzle::get(int row, int col){
+
+    return puzzleTable[row][col];
 }
 
 /** Returns the number of variable entries in the origianl Puzzle.
-* @return int the number of variable entries in this Puzzle
-*/
+ * @return int the number of variable entries in this Puzzle
+ */
 int Puzzle::size() const{
     return emptyVarCount;
 }
 
-/** Returns the number of empty Squares remains in this Puzzle.  
-* @return int the number of empty Squares. 
-*/
+/** Returns the number of empty Squares remains in this Puzzle.
+ * @return int the number of empty Squares.
+ */
 int Puzzle::numEmpty() const{
     return remainVarCount;
 }
 
 /** Return true if the specified value is not euqal to any value
-* in the same row, column, and block, false otherwise.
-* To calculate the location of the left corner of the current block,
-* blockX = (x / 3) * 3, blockY = (y / 3) * 3.
-* @pre value must be in range from 0 to 9.
-* @param x the column number of the current column
-* @param y the row number of the current row
-* @param value the specified value
-* @return bool true if the specifed value is not equal to any value
-* in the same row, column, and block, false othewise. 
-*/
-bool Puzzle::isLegal(int y, int x, int value){
+ * in the same row, column, and block, false otherwise.
+ * To calculate the location of the left corner of the current block,
+ * blockX = (x / 3) * 3, blockY = (y / 3) * 3.
+ * @pre value must be in range from 0 to 9.
+ * @param row the row number of the current column
+ * @param col the col number of the current row
+ * @param value the specified value
+ * @return bool true if the specifed value is not equal to any value
+ * in the same row, column, and block, false othewise.
+ */
+bool Puzzle::isLegal(int row, int col, int value){
     // always legal to set it to 0 unless it's fixed
-    if(puzzleTable[y][x].getIsFixed() == true){
+    if(puzzleTable[row][col].getIsEmpty() == false){
         return false;
     }
     if(value == 0){
@@ -119,78 +121,102 @@ bool Puzzle::isLegal(int y, int x, int value){
     }
     else{
         // calculate the location of the left corner in the current block
-        int blockX = (x / 3) * 3;
-        int blockY = (y / 3) * 3;
+        int blockCol = (col / 3) * 3;
+        int blockRow = (row / 3) * 3;
+        
         // check row, col, block
-        return isRowLegal(y, x, value)
-            && isColumnLegal(y, x, value)
-            && isBlockLegal(blockY, blockX, value);
+        return isRowLegal(row, value)
+            && isColumnLegal(col, value)
+            && isBlockLegal(blockRow, blockCol, value);
     }
     return true;
 }
 
-/** Returns true if sucessfully set the specified value at the 
-* specified location, false otherwise.
-* Precondition: value must be legal before set. 
-* Retruns false if value is illegal. 
-* @pre value must be in range from 0 to 9. 
-* @param x the column number of the specified location
-* @param y the row number of the specified location
-* @param value the specified value
-* @return bool true if successfully set the specified value at
-* the specified location, false otherwise
-*/
-bool Puzzle::set(int y, int x, int value){
+/** Returns true if sucessfully set the specified value at the
+ * specified location, false otherwise.
+ * throws an out of range exception if value is less than 0
+ * and greater than 9.
+ * Precondition: value must be legal before set.
+ * Retruns false if value is illegal.
+ * @pre value must be in range from 0 to 9.
+ * @param row the row number of the specified location
+ * @param col the column number of the specified location
+ * @param value the specified value
+ * @return bool true if successfully set the specified value at
+ * the specified location, false otherwise
+ */
+bool Puzzle::set(int row, int col, int value){
     
     // check value is in range
     if(value < 0 || value > 9){
         throw out_of_range("value is out of range.");
     }
     // if not legal or is a fixed number, set fails
-    if(!isLegal(x, y, value) ||
-       puzzleTable[y][x].getIsFixed() == true){
+    if(isLegal(row, col, value) == false ||
+       puzzleTable[row][col].getIsFixed() == true){
+        cout << "should not get here. " << endl;
         return false;
     }
     else{
         // update remaining empty squares
-        // remain number is not updating correctly
-        if(value == 0){
-            remainVarCount++;
+     
+        Square temp;
+        temp = puzzleTable[row][col];
+
+        // if it's reading in a number for the first time
+        if(temp.getValue() == -1 && value == 0){
+
+                remainVarCount++;
+            
         }
-        else if(puzzleTable[y][x].getIsFixed() == false){
-            remainVarCount--;
+        // if it's changing values
+        else if(temp.getValue() != -1){
+            // set value from non 0 to 0
+            if(value == 0){
+           
+                remainVarCount++;
+            }
+            else{
+                remainVarCount--;
+            }
         }
-        puzzleTable[y][x].setValue(value);
+        
+        puzzleTable[row][col].setValue(value);
+        
     }
+    
     
     return true;
 }
 
 /** Reads input from istream and stores it in a 2D array of Squares.
-* @param input an istream input reference
-* @param thePuzzle reference of this Puzzle 
-* @return istream& the processed istream
-*/
+ * @param input an istream input reference
+ * @param thePuzzle reference of this Puzzle
+ * @return istream& the processed istream
+ */
 istream & operator >>(istream& input, Puzzle& thePuzzle){
     
     // input can't ignore non digit - bug
-    for(int row = 0; row < 9; row++){
-        for(int col = 0; col < 9; col++){
+    for(int row = 0; row < PUZZLE_LENGTH; row++){
+        for(int col = 0; col < PUZZLE_LENGTH; col++){
             char temp = input.get();
-            if(temp < '0' || temp > '9'){
-                temp = input.get();
-            }
-            else if(temp == '\n' || temp == '\r'){
-            }
-            else{
+
+            if(temp >= '0' && temp <= '9' ){
                 int i = temp - '0';
+ 
+                thePuzzle.set(row, col, i);
+            
                 // update emptyVarCount
                 if(i == 0){
                     thePuzzle.emptyVarCount++;
+                    thePuzzle.puzzleTable[row][col].setIsFixed(false);
                 }
-                //thePuzzle.puzzleTable[row][col].setValue(i);
-                thePuzzle.set(row, col, i);    // set does not work
-                thePuzzle.puzzleTable[row][col].setIsFixed(true);
+                else{
+                //
+                    // set does not work
+                    thePuzzle.puzzleTable[row][col].setIsFixed(true);
+                }
+                
             }
 
         }
@@ -200,10 +226,10 @@ istream & operator >>(istream& input, Puzzle& thePuzzle){
 }
 
 /** Returns a string representation of the Puzzle.
-* @param output an ostream output reference
-* @param thePuzzle reference of this Puzzle 
-* @return string a string representation of the Puzzle.
-*/
+ * @param output an ostream output reference
+ * @param thePuzzle reference of this Puzzle
+ * @return string a string representation of the Puzzle.
+ */
 ostream& operator <<(ostream& output, Puzzle& thePuzzle){
     string line = "+-----------------------+";
     cout << line << endl;
@@ -214,14 +240,14 @@ ostream& operator <<(ostream& output, Puzzle& thePuzzle){
         for(int col = 0; col < PUZZLE_LENGTH; col++){
             cout << thePuzzle.puzzleTable[row][col].getValue() << " ";
             rowCounter++;
-            if(rowCounter == 3){
+            if(rowCounter == BLOCK_LENGTH){
                 cout << "| ";
                 rowCounter = 0;
             }
         }
         cout << endl;
         colCounter++;
-        if(colCounter == 3){
+        if(colCounter == BLOCK_LENGTH){
             cout << line << endl;
             colCounter = 0;
         }
@@ -230,5 +256,6 @@ ostream& operator <<(ostream& output, Puzzle& thePuzzle){
     
     return output;
 }
+
 
 
